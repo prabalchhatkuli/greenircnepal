d8489ecbf45ba684290a6ae16aec608108b848b7
Prabal Chhatkuli <pchhatku@ramapo.edu>
2025-07-02 01:15:41 -0400

    Revert "Configure Auth0 with environment variables for Decap CMS"
    This reverts commit 3221fdd831bf796f57e15f1feb2da3292280bc80.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,24 +7,6 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-// Configure Auth0 authentication if environment variables are available
-if (typeof window !== 'undefined' && window.location.hostname !== 'localhost') {
-  // Only initialize Auth0 in production/staging environments
-  const auth0Config = {
-    domain: process.env.GATSBY_AUTH0_DOMAIN,
-    clientId: process.env.GATSBY_AUTH0_CLIENT_ID,
-    audience: process.env.GATSBY_AUTH0_AUDIENCE,
-    scope: 'openid profile email'
-  }
-  
-  // Only configure if all required environment variables are present
-  if (auth0Config.domain && auth0Config.clientId && auth0Config.audience) {
-    CMS.registerBackend('auth0', {
-      ...auth0Config
-    })
-  }
-}
-
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
